0bc0fe88e9055de3550e0bdc16cc856b481846b6
gagpa <speitshot@gmail.com>
2020-11-25 00:08:04 +0300

    Добавлены новые переменные окржуения для подключения к брокеру. Расширен файл settings под подключение к брокеру.

--- parser_news/configs/settings.py
+++ parser_news/configs/settings.py
@@ -9,3 +9,21 @@ import os
 DB_NAME = os.getenv('DB_NAME')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
+
+
+# Настройки подключения к брокеру
+
+# Протокол брокера
+BROKER_PROTOCOL = os.getenv('BROKER_PROTOCOL')
+
+# Пользователь брокера
+BROKER_USER = os.getenv('BROKER_USER')
+
+# Пароль от пользователя
+BROKER_PASSWORD = os.getenv('BROKER_PASSWORD')
+
+# Хост брокера
+BROKER_HOST = os.getenv('BROKER_HOST')
+
+# Порт брокера
+BROKER_PORT = os.getenv('BROKER_PORT')
