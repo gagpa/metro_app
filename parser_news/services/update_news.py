73b20863eb540ad97d646d00140f768d57c2e9d4
gagpa <speitshot@gmail.com>
2020-11-25 00:10:31 +0300

    Логика обновления БД перенсена в модуль сервисов.

---
+++ parser_news/services/update_news.py
@@ -0,0 +1,22 @@
+
+from db.models import News
+from parser.parser_news_list import ParserNewsList
+
+
+def update_news():
+    news = News.objects.order_by('id').first()
+    if news:
+        last_date = news.published_date
+    else:
+        last_date = None
+
+    parser = ParserNewsList()
+
+    while True:
+
+        news = parser.parse(last_date)
+        for n in news:
+            News(**n).save()
+
+        if len(news) == 0:
+            break
